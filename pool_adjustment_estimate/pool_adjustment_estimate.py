bbef03687e8dd6f8f53d2bd7a794896aaa3c1ef3
george-aj <35270943+george-aj@users.noreply.github.com>
2022-06-19 18:35:33 -0500

    Update pool_adjustment_estimate.py

--- pool_adjustment_estimate/pool_adjustment_estimate.py
+++ pool_adjustment_estimate/pool_adjustment_estimate.py
@@ -47,7 +47,7 @@ def parse_gamm_estimates():
 def calculate_airdrop_amount(sender, sender_gamm_map, airdrop_amount_map, gamm_total_share_map, gamm_estimates_map, validation_map):
     sender_airdrop_map = airdrop_amount_map.get(sender) or {}
 
-    #Unknown account that owns gamm that throws off all the numbers
+    #Lockup module account, excluded per: https://docs.osmosis.zone/developing/osmosis-core/modules/#module-accounts
     excluded_sender = 'osmo1njty28rqtpw6n59sjj4esw76enp4mg6g7cwrhc'
     if sender == excluded_sender:
         return
